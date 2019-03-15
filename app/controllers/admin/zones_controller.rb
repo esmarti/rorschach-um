8f9e0490d11affa2f08bd6d8e9720f321431c0a7
Edgardo Marti <edgardos.marti@gmail.com>
2019-03-15 08:41:40 -0300

    Controller Zones: actions completas (MVC) Create, new.

--- app/controllers/admin/zones_controller.rb
+++ app/controllers/admin/zones_controller.rb
@@ -22,37 +22,57 @@ class Admin::ZonesController < ApplicationController
 		@sheet = Sheet.find_by_id(zone_params[:sheet_id])
 	end
 
-  def update
-    respond_to do |format|
-      if @zone.update(edit_params)
-        format.html { redirect_to admin_sheet_area_zone_path(@zone), notice: 'Zone was successfully updated.' }
-        format.json { render :show, status: :ok, location: admin_sheet_area_zone_path(@zone) }
-      else
-        format.html { render :edit }
-        format.json { render json: @zone.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	def update
+		respond_to do |format|
+			if @zone.update(edit_params)
+				format.html { redirect_to admin_sheet_area_zone_path(@zone), notice: 'Zone was successfully updated.' }
+				format.json { render :show, status: :ok, location: admin_sheet_area_zone_path(@zone) }
+			else
+				format.html { render :edit }
+				format.json { render json: @zone.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	def new
+		@zone = Zone.new
+	end
+
+	def create
+		@zone = Zone.new(edit_params)
+		@area = Area.find_by_id(zone_params[:area_id])
+		@sheet = Sheet.find_by_id(zone_params[:sheet_id])
+
+		respond_to do |format|
+			if @zone.save
+				format.html { redirect_to admin_sheet_area_zone_path(@zone), notice: 'Zone was successfully created.' }
+				format.json { render :show, status: :created, location: admin_sheet_area_zone_path(@zone) }
+			else
+				format.html { render :new }
+				format.json { render json: @zone.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
 	def destroy
-	  @zone.destroy
-	  respond_to do |format|
-	    format.html { redirect_to admin_sheet_area_zone_url(zone_params[:sheet_id]) , notice: 'Zone was successfully destroyed.' }
-	    format.json { head :no_content }
-	  end
+		@zone.destroy
+		respond_to do |format|
+			format.html { redirect_to admin_sheet_area_zone_url(zone_params[:sheet_id]) , notice: 'Zone was successfully destroyed.' }
+			format.json { head :no_content }
+		end
 	end
 
 	private
 		# Use callbacks to share common setup or constraints between actions.
 		def set_zone
-		  @zone = Zone.find(zone_params[:id])
+			@zone = Zone.find(zone_params[:id])
 		end
 
 		# Never trust parameters from the scary internet, only allow the white list through.
 		def zone_params
-		  params.permit(:sheet_id, :area_id, :id)
+			params.permit(:sheet_id, :area_id, :id)
 		end
 		def edit_params
-		  params.require(:zone).permit(:coord_x, :coord_y, :width, :height, :z_index, :bg_image)
+			params.require(:zone).permit(:coord_x, :coord_y, :width, :height, :z_index, :bg_image)
 		end
 end
