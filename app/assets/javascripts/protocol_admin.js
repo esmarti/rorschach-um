5025d889e8b2beef378f9c0a1d180fbd32f5966a
Edgardo Marti <edgardos.marti@gmail.com>
2019-07-04 09:56:17 -0300

    protocol admin: javascript añadido para permitir seleccionar areas con los areaButtons. Boton Reestablecer funcionando.

--- app/assets/javascripts/protocol_admin.js
+++ app/assets/javascripts/protocol_admin.js
@@ -1,20 +1,5 @@
 function reset_areas() {
-	//select img elements
-    var elements=$("#box img");
-
-    //change style to "default" (not selected)
-    for(var i=0; i<elements.length; i++){
-    	if(elements[i].id.match(/capa[0-9]{1,2}/)){
-    		elements[i].className="area";
-    	}
-    }
+	//select img elements and remove selected style
+    var elements=$( "#box img.area" ).removeClass("proposed selected");
     return;
 }
-
- $("#areaButtons button").on({
- 	hover: function(event){
- 		for(i=0; i<this.attributes[1].size; i++) {
- 			console.log(this.attributes[1]);	
- 		}
- 	}
- });
--- app/assets/javascripts/sheets.js
+++ app/assets/javascripts/sheets.js
@@ -1,12 +1,36 @@
 //function for colour areas when mouse is over button in "area selector"
-$(document).ready(function(){
-	$("#areaButtons button").hover(function() {
+$(window).on("load",function(){
+	//mostrar un area al pasar con el mouse sobre el areaButton
+	$("#areaButtons button").mouseenter(function() {
 		//buscar el boton sobre el que ocurrio el event hover y guardar el attr "arr_zones".
 		// este attr corresponde a los id de las zonas que corresponden a un area (attr id del button areaButtons)
   		var areas_arr=JSON.parse(this.attributes[1].value.toString());
   		//buscar todos los elementos de tipo area de la vista y cambiarles el stilo
   		for(var i=0; i<areas_arr.length+0; i++) {
-  			$("#box #l_"+areas_arr[i]).toggleClass('proposed');
+  			$("#box #l_"+areas_arr[i]).addClass('proposed');
   		}
+  	});
+  	$("#areaButtons button").mouseout(function() {
+		//buscar el boton sobre el que ocurrio el event hover y guardar el attr "arr_zones".
+		// este attr corresponde a los id de las zonas que corresponden a un area (attr id del button areaButtons)
+  		var areas_arr=JSON.parse(this.attributes[1].value.toString());
+  		//buscar todos los elementos de tipo area de la vista y cambiarles el stilo
+  		for(var i=0; i<areas_arr.length+0; i++) {
+  			$("#box #l_"+areas_arr[i]).removeClass('proposed');
+  		}
+  	});
+
+	//seleccionar un area completa desde el areaButton y colorearla
+  	$("#areaButtons button").mousedown(function(evento) {
+  		reset_areas();
+  		evento.stopImmediatePropagation();
+		//buscar el boton sobre el que ocurrio el event hover y guardar el attr "arr_zones".
+		// este attr corresponde a los id de las zonas que corresponden a un area (attr id del button areaButtons)
+  		var areas_arr=JSON.parse(this.attributes[1].value.toString());
+  		//buscar todos los elementos de tipo area de la vista y cambiarles el stilo
+  		for(var i=0; i<areas_arr.length+0; i++) {
+  			$("#box #l_"+areas_arr[i]).toggleClass('selected');
+  		}
+  		//setear el estado del area como "seleccionada"
   	});
 });
